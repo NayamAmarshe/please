8a494212c0842ef024073e0fc02ab9e0331ea56a
Feenix <25067102+NayamAmarshe@users.noreply.github.com>
2022-05-10 14:59:03 +0530

    Changed tasks implementation

--- please/please.py
+++ please/please.py
@@ -22,18 +22,18 @@ state = {"verbose": False}
 config_path = os.path.join(expanduser("~"), ".config", "please")
 if not os.path.exists(config_path):
     os.makedirs(config_path)
-else: # ONLY ATTEMPT TO READ THE FILE OF THE FOLDER EXISTS
-    try:
-        cfile = open(os.path.join(config_path, "config.json"))
-        config = json.load(cfile)
-        cfile.close()
-    except Exception:
-        print("The setup was not complete, please do the setup again") # IF THE SETUP HAD COMPLETED FULLY, THE FILE SHOULD HAVE BEEN CREATED
 
-# FUNCTION TO WRITE THE JSON DATA TO THE CONFIG FILE
+try:  # Try reading the config.json file
+    with open(os.path.join(config_path, "config.json")) as config_file:
+        config = json.load(config_file)  # Set config variable to json data
+except:  # If it doesn't exist, create a new config.json file
+    open(os.path.join(config_path, "config.json"), "w")
+
+
 def write_config(data):
     with open(os.path.join(config_path, "config.json"), 'w') as of:
-        of.write(json.dumps(data, indent = 2)) #THIS IS DONE SO THE THE JSON DATA IS NOT WRITTEN AS A SINGLE LINE
+        of.write(json.dumps(data, indent=2))  # 'indent' formats json properly
+
 
 def get_time_of_day(x):
     if (x > 4) and (x <= 12):
@@ -49,46 +49,59 @@ def get_time_of_day(x):
 @ app.command(short_help="Setup Wizard for First Time Run")
 def callme(name: str):
     config["user_name"] = name
+    write_config(config)
     typer.echo(f"Thanks for letting me know your name!")
 
-# TASK FUNCTIONS START
 
-# FUNCTION TO ADD A TASK
 @ app.command(short_help='Add a Task')
 def add(task: str):
-    config["tasks"][task] = "❌"
+    new_task = {
+        "name": task,
+        "done": False
+    }
+    config["tasks"].append(new_task)
     write_config(config)
-    typer.echo(f"Added => {task} <= to the list")
-    show_table(config["tasks"]) # PRINTING THE TABLE TO SHOW UPDATED ENTRIES
+    typer.echo(f"Added \"{task}\" to the list")
+    showtasks(config["tasks"])
 
-# FUNCTION TO DELETE A TASK
+
 @ app.command(short_help='Deletes a Task')
-def delete(task: str):
-    config["tasks"].pop(task)
-    write_config(config)
-    typer.echo(f"Deleted {task}")
-    show_table(config["tasks"]) # PRINTING THE TABLE TO SHOW UPDATED ENTRIES
+def delete(index: int):
+    if len(config["tasks"]) > 0:
+        deleted_task = config["tasks"][index]
+        del config["tasks"][index]
+        write_config(config)
+        typer.echo(f"Deleted {deleted_task}")
+        showtasks(config["tasks"])
+    else:
+        print("Sorry, I've got no tasks to delete")
 
-# FUNCTION TO MARK A TASK AS DONE
+
 @ app.command(short_help='Marks a task as done')
 def done(task: str):
     config["tasks"][task] = "✔️"
     write_config(config)
     typer.echo(f"Updated Task List")
-    show_table(config["tasks"]) # PRINTING THE TABLE TO SHOW UPDATED ENTRIES
+    showtasks(config["tasks"])
 
-# FUNCTION TO PRINT THE TABLE
-def show_table(task_dict):
-    table1 = Table(show_header=True, header_style='bold')
-    table1.add_column('Task')
-    table1.add_column('Status')
-    for task, status in task_dict.items():
-        table1.add_row(task, status)
-    # PRINTING THE TABLE (COULD BE MADE PRETTIER)
-    console.print(table1)
-# TASK FUNCTIONS END
 
-@ app.command(short_help="First run setup wizard")
+def showtasks(tasks_list):
+    if len(tasks_list) > 0:
+        table1 = Table(show_header=True, header_style='bold')
+        table1.add_column('Number')
+        table1.add_column('Task')
+        table1.add_column('Status')
+        for index, task in enumerate(tasks_list):
+            task_name = task["name"]
+            task_status = "✅" if task["done"] else "❌"
+            table1.add_row(str(index), task_name, task_status)
+        # PRINTING THE TABLE (COULD BE MADE PRETTIER)
+        console.print(table1)
+    else:
+        print("Looking good, no tasks 😁")
+
+
+@ app.command(short_help="Reset data and run Setup Wizard")
 def setup():
     # SETUP WIZARD
     # ASK FOR USERNAME
@@ -108,35 +121,30 @@ def setup():
 
     # SET DEFAULT VARIABLES IN JSON DATASTORE
     config["initial_setup_done"] = True
-    config["tasks"] = {}
+    config["tasks"] = []
     write_config(config)
 
+
 @ app.callback(invoke_without_command=True)
-def show(date: bool = True, name: bool = True, tasks_list: bool = True):
+def show():
     dateNow = datetime.datetime.now()
 
-    if name == True:
-        user_name = config["user_name"]
-        time_of_day = get_time_of_day(int(dateNow.strftime("%H")))
-        typer.secho(art.text2art(f"{time_of_day} {user_name}!", "tarty7"),
-                    fg=typer.colors.YELLOW)
+    user_name = config["user_name"]
+    time_of_day = get_time_of_day(int(dateNow.strftime("%H")))
+    typer.secho(art.text2art(f"{time_of_day} {user_name}!", "tarty7"),
+                fg=typer.colors.YELLOW)
 
-    if date == True:
-        # TODO: POSSIBLY DELETE THIS AND REPLACE WITH BASH INSTEAD
-        dateNow = datetime.datetime.now()
-        typer.secho(art.text2art(
-            dateNow.strftime("%d %b == %I:%M %p"), "thin3"), fg=typer.colors.MAGENTA)
-    if tasks_list == True:
-        show_table(config["tasks"])
+    # TODO: POSSIBLY DELETE THIS AND REPLACE WITH BASH INSTEAD
+    dateNow = datetime.datetime.now()
+    typer.secho(art.text2art(
+        dateNow.strftime("%d %b == %I:%M %p"), "thin3"), fg=typer.colors.MAGENTA)
+
+    showtasks(config["tasks"])
 
 
 if __name__ == "__main__":
-    try:
-        # IF CONFIG ALREADY HAS INITIAL_SETUP_DONE TO TRUE, FIRE THE APP
-        with open(os.path.join(config_path, "config.json")) as cfile:
-            store = json.load(cfile)
-            if(store["initial_setup_done"] == True):
-                app()
-    except Exception:
-        # IF CONFIG DOESN'T EXIST, CALL THE SETUP WIZARD
+    # IF CONFIG ALREADY HAS INITIAL_SETUP_DONE TO TRUE, FIRE THE APP
+    if(config["initial_setup_done"] == True):
+        app()
+    else:
         typer.run(setup)
